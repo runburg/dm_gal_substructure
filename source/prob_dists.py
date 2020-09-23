49a72a485121a016638b1fe78f2680086fc19f37
Jack Runburg <jack.runburg@gmail.com>
2020-09-22 14:36:59 -1000

    Semi-working analysis

--- source/prob_dists.py
+++ source/prob_dists.py
@@ -8,8 +8,9 @@ Description: Probability distributions and helper functions for DM annihilating 
 """
 import numpy as np
 from scipy import integrate, stats
-from scipy.interpolate import interp1d, interp2d
+from scipy.interpolate import interp2d
 import plots
+from scipy import interpolate as intp
 import random
 
 
@@ -279,10 +280,88 @@ def psh_of_psi(params, psi_min=40, psi_max=180, num_psi=14, plot=False, infer_va
     return np.array(psh_vals_over_psi), np.array(fluxes), psis
 
 
+def psh_s(ang_dists, input_file='./output/n0_pshfunc.npz', return_all=False):
+    """
+    Compute P_sh for sommerfeld annihilation.
+    """
+    with np.load(input_file) as f:
+        psi = f['psi']
+        fluxes = f['flux']
+        psh2d = f['psh']
+
+    # restrict to valid range of flux calculation
+    low_lim = 0
+    valid_lim = -55
+    fluxes = fluxes[low_lim:valid_lim]
+    psh2d = psh2d[low_lim:valid_lim]
+
+    # get psh data as function of psi and flux
+    psh2d[psh2d < 0] = 0
+    pshfunc2d = intp.interp2d(psi, fluxes, np.nan_to_num(psh2d), bounds_error=False, fill_value=0)
+
+    # get psh for the angles we are considering
+    psh = pshfunc2d(np.abs(ang_dists), fluxes)
+    psh /= np.trapz(psh, fluxes, axis=0)
+
+    if return_all is True:
+        return psh, pshfunc2d, fluxes, psi
+
+    return psh
+
+
+def psh_som(ang_dists, input_file='./output/n-1_pshfunc.npz', return_all=False):
+    """
+    Compute P_sh for sommerfeld annihilation.
+    """
+    with np.load(input_file) as f:
+        psi = f['psi']
+        fluxes = f['flux']
+        psh2d = f['psh']
+
+    # restrict to valid range of flux calculation
+    low_lim = 150
+    valid_lim = -50
+    fluxes = fluxes[low_lim:valid_lim]
+    psh2d = psh2d[low_lim:valid_lim]
+
+    # get psh data as function of psi and flux
+    psh2d[psh2d < 0] = 0
+    pshfunc2d = intp.interp2d(psi, fluxes, np.nan_to_num(psh2d), bounds_error=False, fill_value=0)
+
+    # get psh for the angles we are considering
+    psh = pshfunc2d(np.abs(ang_dists), fluxes)
+    psh /= np.trapz(psh, fluxes, axis=0)
+
+    if return_all is True:
+        return psh, pshfunc2d, fluxes, psi
+
+    return psh
+
+
+def pc_of_psi(p, pshfunc2d, fluxes, counts, bg_count=np.array([0])):
+    psis = np.linspace(40, 180, num=60)
+
+    f = p['fwimp']
+    exposure = p['exposure']
+
+    mean_f = f * np.trapz(fluxes * pshfunc2d(psis[0], fluxes).flatten(), fluxes)
+    mean_count = int(mean_f * exposure + np.max(bg_count))
+    counts = np.arange(0, mean_count + 4 * np.sqrt(mean_count) + 40)
+    if counts[-1] > 50:
+        print('largest count is', counts[-1], '...this may take a while')
+
+    pc_psi = np.trapz(1/f * pshfunc2d(psis, fluxes)[:, :, np.newaxis] * stats.poisson.pmf(counts[np.newaxis, np.newaxis, :], exposure * f * fluxes[:, np.newaxis, np.newaxis] + bg_count[np.newaxis, :, np.newaxis]), f * fluxes, axis=0)
+
+    pc_of_psi = intp.interp1d(psis, np.array(pc_psi, dtype=np.float64), axis=0)
+
+    if np.any(np.isnan(pc_psi)):
+        print('found nan in pc')
+
+    return pc_of_psi
+
+
 def interp_and_save_psh(params, psh, flux, psi, return_interp2d=True, outfile='./output/pshfunc.npz'):
     """Get a convenient form for psh and save arrays."""
-    from scipy import interpolate as intp
-
     fluxes = np.logspace(params['psh_log_f_min'], params['psh_log_f_max'], num=250)
     interp_array = np.zeros((len(fluxes), len(psi)))
 
@@ -308,9 +387,90 @@ def pc(psh_2dfunc, fluxes, psi, exposure, background=False, countmax=20):
     from scipy.stats import poisson
 
     counts = np.arange(0, countmax + 1)
-    pcvals = np.trapz(pshfunc2d(psi, fluxes).flatten() * poisson.pmf(counts[:, np.newaxis], exposure * fluxes), fluxes, axis=-1)
+    pcvals = np.trapz(psh_2dfunc(psi, fluxes).flatten() * poisson.pmf(counts[:, np.newaxis], exposure * fluxes), fluxes, axis=-1)
 
     return pcvals, counts
+
+
+def generate_skymap_sample_pc_2(p, pshfunc2d, fluxes, cut_out_band=40, output_path='./output/', print_updates=False, save_output=False, return_subcounts=False, with_bg=True):
+    import healpy
+    f = p['fwimp']
+    exposure = p['exposure']
+    nside = p['nside']
+    npix = healpy.nside2npix(nside)
+    pixel_counts = np.ones(npix) * healpy.pixelfunc.UNSEEN
+
+    lon, lat = healpy.pix2ang(nside, range(npix), lonlat=True)
+
+    good_indices = (abs(lat) >= cut_out_band)
+
+    ang_dists = np.rad2deg(np.arccos(np.cos(np.deg2rad(lon[good_indices])) * np.cos(np.deg2rad(lat[good_indices]))))
+
+    sub_counts = np.zeros(len(ang_dists))
+
+    if with_bg is True:
+        gal_bg = np.load(p['gal_flux_bg_file'])[good_indices] * p['exposure']
+        iso_bg = p['iso_flux_bg'] * p['exposure']
+        # print(np.mean(gal_bg), iso_bg)
+        bg_counts = gal_bg + iso_bg
+        print('got background', np.mean(bg_counts))
+    else:
+        bg_counts = 0
+        gal_bg = 0
+        iso_bg = 0
+
+    mean_f = f * np.trapz(fluxes[:, np.newaxis] * pshfunc2d(np.abs(ang_dists), fluxes), fluxes[:, np.newaxis], axis=0)
+    mean_f *= exposure
+    mean_f += bg_counts
+    # nominal_counts = np.arange(int(-2*np.std(mean_f)), int(2*np.std(mean_f)))
+    nominal_counts = np.arange(-40, 40)
+    print('count len', len(nominal_counts))
+    counts = nominal_counts[np.newaxis, :].astype(np.int16) + mean_f[:, np.newaxis].astype(np.int16)
+    counts[counts<0] = 0
+
+    # mean_f = f * np.trapz(fluxes * pshfunc2d(np.abs(ang_dists).min(), fluxes).flatten(), fluxes)
+    # mean_count = (mean_f * exposure + np.mean(bg_counts)).astype(np.int64)
+    # lower_count = mean_count
+    # lower_count -= 4 * np.sqrt(lower_count)
+    # if lower_count < 0:
+    #     lower_count = 0
+    # upper_count = mean_count
+    # upper_count += 4 * np.sqrt(upper_count)
+    # counts = np.arange(lower_count, upper_count)
+    # if counts[-1] > 50:
+    #     print('largest count is', counts[-1], '...this may take a while')
+
+    pcvals = np.trapz(1/f * pshfunc2d(ang_dists, fluxes)[:, :, np.newaxis] * stats.poisson.pmf(counts[np.newaxis, :, :], exposure * f * fluxes[:, np.newaxis, np.newaxis] + bg_counts[np.newaxis, :, np.newaxis]), f * fluxes, axis=0)
+    
+    if np.any(np.sum(pcvals, axis=-1) < 0.9):
+        print('calculated pc does not integrate to 1. potentially due to insufficient count range. consider increasing the min/max allowed counts', np.sum(np.sum(pcvals, axis=-1) < 0.9))
+    pcvals /= np.sum(pcvals, axis=-1)[:, np.newaxis]
+
+    cum_pc = pcvals.cumsum(axis=1)
+    rand_vals = np.random.rand(len(cum_pc), 1)
+    sub_counts = (rand_vals < cum_pc).argmax(axis=1)
+    sub_counts += counts[:, 0]
+    # sub_counts += lower_count
+
+    # for i, psi in enumerate(ang_dists):
+    #     if print_updates is True:
+    #         if i % 10000 == 0:
+    #             print(i, '/', len(ang_dists))
+    #     #     print(psi)
+    #     pcvals = pc_of_psi(abs(psi))
+    #     sub_counts[i] += np.random.choice(np.arange(len(pcvals)), size=1, p=pcvals/np.sum(pcvals))
+
+    pixel_counts[good_indices] = sub_counts
+
+    if save_output is True:
+        outfile = f"{output_path}n{p['n']}_skymap_{str(random.randint(0, 99999)).rjust(5, '0')}.npy"
+        np.save(outfile, pixel_counts)
+        print("saved in", outfile)
+
+    if return_subcounts is True:
+        return sub_counts, ang_dists, gal_bg + iso_bg
+
+    return pixel_counts
 
 
 def generate_skymap_sample_pc(p, pc_of_psi, cut_out_band=40, output_path='./output/', print_updates=False, save_output=False, return_subcounts=False, with_bg=True):
@@ -333,9 +493,18 @@ def generate_skymap_sample_pc(p, pc_of_psi, cut_out_band=40, output_path='./outp
         bg_counts = stats.poisson.rvs(gal_bg + iso_bg)
     else:
         bg_counts = 0
+        gal_bg = 0
+        iso_bg = 0
 
     sub_counts = np.zeros(len(ang_dists))
     sub_counts += bg_counts
+
+    # pcvals = pc_of_psi(np.abs(ang_dists))
+    # pcvals /= np.sum(pcvals, axis=-1)[:, np.newaxis]
+
+    # cum_pc = pcvals.cumsum(axis=1)
+    # rand_vals = np.random.rand(len(cum_pc), 1)
+    # sub_counts += (rand_vals < pcvals).argmax(axis=1)
 
     for i, psi in enumerate(ang_dists):
         if print_updates is True:
@@ -345,7 +514,7 @@ def generate_skymap_sample_pc(p, pc_of_psi, cut_out_band=40, output_path='./outp
         pcvals = pc_of_psi(abs(psi))
         sub_counts[i] += np.random.choice(np.arange(len(pcvals)), size=1, p=pcvals/np.sum(pcvals))
 
-        pixel_counts[good_indices] = sub_counts
+    pixel_counts[good_indices] = sub_counts
 
     if save_output is True:
         outfile = f"{output_path}n{p['n']}_skymap_{str(random.randint(0, 99999)).rjust(5, '0')}.npy"
@@ -353,12 +522,12 @@ def generate_skymap_sample_pc(p, pc_of_psi, cut_out_band=40, output_path='./outp
         print("saved in", outfile)
 
     if return_subcounts is True:
-        return sub_counts, ang_dists
+        return sub_counts, ang_dists, gal_bg + iso_bg
 
     return pixel_counts
 
 
-def likelihood(p, psh, subcounts, fluxes, counts, fwimp_limits=(-5, 5, 20), bg_count=0):
+def likelihood(p, psh, subcounts, fluxes, counts, fwimp_limits=(-5, 5, 20), bg_count=np.array([0]), verbose=False):
     from scipy.stats import poisson
 
     exposure = p['exposure']
@@ -368,36 +537,46 @@ def likelihood(p, psh, subcounts, fluxes, counts, fwimp_limits=(-5, 5, 20), bg_c
     S = np.zeros(fwimps.shape)
 
     for i, f in enumerate(fwimps):
-        print(i, '/', len(fwimps))
+        if verbose is True:
+            print(i, '/', len(fwimps))
         # p['fwimp'] = f
 
-        pc = np.trapz(1 / f * psh[:, :, np.newaxis] * poisson.pmf(counts[np.newaxis, np.newaxis, :], exposure * f * fluxes[:, np.newaxis, np.newaxis] + bg_count[np.newaxis, :, np.newaxis]), f * fluxes, axis=0)
+        pc = integrate.simps(1/f * psh * poisson.pmf(subcounts[np.newaxis, :], exposure * f * fluxes[:, np.newaxis] + bg_count[np.newaxis, :]), f * fluxes, axis=0)
+        # pc = np.trapz(1/f * psh * poisson.pmf(subcounts[np.newaxis, :], exposure * f * fluxes[:, np.newaxis] + bg_count[np.newaxis, :]), f * fluxes, axis=0)
+        # pc = np.trapz(1 / f * psh[:, :, np.newaxis] * poisson.pmf(counts[np.newaxis, np.newaxis, :], exposure * f * fluxes[:, np.newaxis, np.newaxis] + bg_count[np.newaxis, :, np.newaxis]), f * fluxes, axis=0)
 
-        pixel_probs = pc[np.arange(len(pc)), subcounts]
+        # print(pc)
+        # pixel_probs = pc[np.arange(len(pc)), subcounts]
+        pixel_probs = pc
 
+        print('zero prob pixels', np.sum(pixel_probs <= 0))
         S[i] = -2 * np.sum(np.log(pixel_probs, where=(pixel_probs > 0)))
 
     return S, fwimps
 
 
-def poisson_likelihood(p, psh, subcounts, fluxes, counts, fwimp_limits=(-5, 5, 20)):
+def poisson_likelihood(p, psh, subcounts, fluxes, counts, fwimp_limits=(-5, 5, 20), bg_count=np.array([0]), verbose=False):
     from scipy.stats import poisson
 
     exposure = p['exposure']
 
     fwimps = np.logspace(*fwimp_limits)
-    print(fwimps)
     S = np.zeros(fwimps.shape)
 
+    mean_f = np.trapz(fluxes[:, np.newaxis] * psh, fluxes[:, np.newaxis], axis=0)
+
     for i, f in enumerate(fwimps):
-        print(i, '/', len(fwimps))
-        p['fwimp'] = f
+        if verbose is True:
+            print(i, '/', len(fwimps))
 
-        pc = poisson.pmf(counts[np.newaxis, :], exposure * f * np.trapz(fluxes[:, np.newaxis] * psh, f * fluxes, axis=0)[:, np.newaxis])
+        pc = poisson.pmf(subcounts, exposure * f * mean_f + bg_count)
 
-        pixel_probs = pc[np.arange(len(pc)), subcounts]
+        # pixel_probs = pc[np.arange(len(pc)), subcounts]
+        pixel_probs = pc
 
-        S[i] = -2 * np.sum(np.log(pixel_probs))
+        print('zero prob pixels', np.sum(pixel_probs <= 0))
+
+        S[i] = -2 * np.sum(np.log(pixel_probs, where=(pixel_probs>0)))
 
     return S, fwimps
 
